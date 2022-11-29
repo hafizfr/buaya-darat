433a68a05688bbe7dba8370e0f131fac3ae59091
hafizfr <hafizfriski0@gmail.com>
2022-11-29 10:05:26 +0700

    Terakhir membuat springs

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1669689057,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundatas.png",
+		"images/backgroundtengah.png",
+		"images/backgroundbawah.png",
+		"images/tanah.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/springs-sheet0.png",
+		"images/springs-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
